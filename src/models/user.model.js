554e2ca1da86694c345a09a360fddde939705d37
om kachhadiya <kachhadiyaom2005@gmail.com>
2025-07-19 12:25:02 +0530

    Added a Bcrypt

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,4 +1,6 @@
 import mongoose, { Schema } from "mongoose";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
 
 const userSchema = new mongoose.Schema({
     username: {
@@ -43,6 +45,19 @@ const userSchema = new mongoose.Schema({
         type: String
     }
 },{timestamps: true});
+
+userSchema.pre("save", async function(next) {
+    if(!this.isModified("password")) {
+        return next();
+    }
+
+    this.password = await bcrypt.hash(this.password,10);
+    next();
+});
+
+userSchema.methods.isPasswordCorrect = async function (password) {
+    return await bcrypt.compare(password,this.password);
+}
 
 const User = mongoose.model("User",userSchema);
 
