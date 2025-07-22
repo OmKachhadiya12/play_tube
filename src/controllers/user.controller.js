8d9120fc5075c891a818c4bfe1e99a1296a94ef2
om kachhadiya <kachhadiyaom2005@gmail.com>
2025-07-22 18:22:55 +0530

    Added more things.

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,7 +1,23 @@
 import {asyncHandler} from "./../utils/asyncHandler.js";
+import {apiError} from "./../utils/apiError.js";
+import User from "./../models/user.model.js";
 
-const register = asyncHandler( async () => {
-    res.ststus(200).json({message:"Done!!!"});
+const registerUser = asyncHandler( async () => {
+    const registerUser = asyncHandler( async(req,res) => {
+        const {fullName,email,username,password} = req.body;
+
+        if([fullName,email,username,password].some((feild)=>{ feild?.trim() == ""})) {
+            throw new apiError(400,"All fields are Required.");
+        }
+
+        const existedUser = User.findOne({
+            $or: [{username},{email}]
+        });
+
+        if(existedUser) {
+            throw new apiError(409,"User already exist.");
+        }
+    } )
 });
 
-export {register};
+export {registerUser};
