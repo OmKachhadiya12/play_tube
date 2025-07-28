e11f0c5ba0792b5c0b50c9401bfa5cde4710695f
om kachhadiya <kachhadiyaom2005@gmail.com>
2025-07-28 12:46:09 +0530

    Added a getUserChannelProfile controller.

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -183,7 +183,7 @@ const changeCurrentPassword = asyncHandler(async(req,res) => {
 
 
 const getCurrentUser = asyncHandler(async(req,res) => {
-  return res.status(200).json(200,req.user,"Current User fetched sucessfully.");
+  return res.status(200).json(new apiResponse(200,req.user,"Current User fetched sucessfully."));
 });
 
 
@@ -235,4 +235,74 @@ const updateUserCoverImage = asyncHandler(async(req,res) => {
 });
 
 
-export { registerUser,loginUser,logoutUser,refreshAccessToken,changeCurrentPassword,getCurrentUser,updateUserAvatar,updateUserCoverImage };
+const getUserChannelProfile = asyncHandler(async(req,res) => {        
+  const {username} = req.params;
+
+  if(!username?.trim()) {
+    throw new apiError(400,"Username is Missing.");
+  }
+
+  const channel = await User.aggregate([
+    {
+      $match: {
+        username: username?.toLowerCase()
+      }
+    },
+    {
+      $lookup: {
+        from: "subscriptions",
+        localField: "_id",
+        foreignField: "channel",
+        as: "subscribers"
+      }
+    },
+    {
+      $lookup: {
+        from: "subscriptions",
+        localField: "_id",
+        foreignField: "subscriber",
+        as: "subscribedTo"
+      }
+    },
+    {
+      $addFields: {
+        subscribersCount: {
+          $size: "$subscribers"
+        },
+        channelSubscribedToCount: {
+          $size: "$subscribedTo"
+        },
+        isSubscribed: {
+          $cond: {
+            $if: {$in: [req.user?._id,"$subscribers.subscriber"]},
+            then: true,
+            else: false
+          }
+        }
+      }
+    },
+      {
+        $project: {
+          fullName: 1,
+          username: 1,
+          email: 1,
+          avatar: 1,
+          coverImage: 1,
+          subscribersCount: 1,
+          channelSubscribedToCount: 1,
+          isSubscribed: 1
+
+        }
+      }
+  ]);
+
+  if(!channel?.length) {
+    throw new apiError(400,"Channel doesn't exist.");
+  }
+
+  return res.status(200).json(new apiResponse(200,,channel[0],"User channel details fetched sucessfully."));
+
+});
+
+
+export { registerUser,loginUser,logoutUser,refreshAccessToken,changeCurrentPassword,getCurrentUser,updateUserAvatar,updateUserCoverImage,getUserChannelProfile };
